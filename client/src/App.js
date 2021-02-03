99983c97ff9a4bd5a8d889b9219e329d74f002e3
Quesnel maxime <maxime.quesnel.contact@gmail.com>
2021-02-03 14:57:04 +0100

    Create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // Redux connect
 import { Provider } from 'react-redux';
 import store from './store';
+
+// Components
+import Navbar from './components/elements/Navbar/Navbar';
+
+// Pages
 import HomePage from './components/pages/HomePage/HomePage';
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path='/' component={HomePage} />
         </Switch>
